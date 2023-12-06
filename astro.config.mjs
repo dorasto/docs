b1bd11402e6cf4bdbd01f855ef0eb1d43dd9a9ae
Tommerty <86270372+tommerty@users.noreply.github.com>
2023-12-06 14:25:03 +0000

    adding cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,47 +11,40 @@ export default defineConfig({
   integrations: [starlight({
     title: 'Doras.to',
     logo: {
-      light:
-        './src/assets/favicon.svg',
-      dark: './src/assets/favicon-white.svg',
+      light: './src/assets/favicon.svg',
+      dark: './src/assets/favicon-white.svg'
     },
-    customCss: [
-      './src/styles/custom.css',
-    ],
+    customCss: ['./src/styles/custom.css'],
     editLink: {
-      baseUrl: 'https://github.com/doras-to/docs/edit/master/',
+      baseUrl: 'https://github.com/doras-to/docs/edit/master/'
     },
     social: {
       github: 'https://github.com/doras-to/docs'
     },
-    sidebar: [
-      {
-        label: 'Guides',
-        items: [
-          // Each item here is one entry in the navigation menu.
-          {
-            label: 'Getting Started',
-            link: '/guides/getting-started/'
-          },
-          {
-            label: 'Migrating from Mystlink',
-            link: '/mystlink/waitlist/'
-          }]
-      },
-      {
-        label: 'Core Features',
-        items: [
-          {
-            label: 'Understanding Links',
-            link: '/links'
-          },
-        ]
-      },
-      {
-        label: 'Contributing',
-        autogenerate: {
-          directory: 'contributing',
-        }
+    sidebar: [{
+      label: 'Guides',
+      items: [
+        // Each item here is one entry in the navigation menu.
+        {
+          label: 'Getting Started',
+          link: '/guides/getting-started/'
+        }, {
+          label: 'Migrating from Mystlink',
+          link: '/mystlink/waitlist/'
+        }]
+    }, {
+      label: 'Core Features',
+      items: [{
+        label: 'Understanding Links',
+        link: '/links'
       }]
-  }), react(), tailwind()]
+    }, {
+      label: 'Contributing',
+      autogenerate: {
+        directory: 'contributing'
+      }
+    }]
+  }), react(), tailwind()],
+  output: "server",
+  adapter: cloudflare()
 });
