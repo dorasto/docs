b7eaed790917ad2daecd282427345d79dd40b383
Tommy <86270372+tommerty@users.noreply.github.com>
2024-04-10 11:51:11 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://docs.doras.to',
+  site: 'https://dorasto.github.io',
+  base: 'docs'
   integrations: [starlight({
     title: 'Doras.to',
     logo: {
